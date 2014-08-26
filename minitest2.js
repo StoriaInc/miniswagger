2b20c0e9e1c50a73ccababc51bf02fb8bdf011dc
Sergey Urzhumskov <kinbot@gmail.com>
2014-08-26 22:57:26 +0600

    more tests (including cookies)

--- minitest2.js
+++ minitest2.js
@@ -7,12 +7,32 @@ var miniswagger = require('miniswagger').default;
 
 var Api = new miniswagger.fromSpecs(specs);
 
-Api.core.getStory({id: '0689a05650017f80'}).then(
-    function(story) {
-        console.log(story);
-    },
-    function(error) {
-        console.error(error);
-    }
-);
+Api.acl.auth({
+    authAccountId: 'sergey.urzhumskov@frumatic.com',
+    password: '123456',
+    authType: 'Selfish',
+    remember: false
+})
+    .then(
+        function(authData) {
+            console.log(authData);
+            return Api.core.getUser({ id: authData.obj.accountId});
+        }
+    )
+    .then(
+        function(profile) {
+            console.log(profile.obj);
+            return Api.core.getStory({id: '0689a05650017f80'});
+        }
+    )
+    .then(
+        function(story) {
+            console.log(story.obj);
+
+            console.log(Api.jar); // check that the auth cookie is set
+        },
+        function(error) {
+            console.error(error);
+        }
+    );
 
